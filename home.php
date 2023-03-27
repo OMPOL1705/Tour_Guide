45e0b4cbc182fe45c200d16100ea5dcdbb2097d5
OM1705 <ompol1705@gmail.com>
2023-03-27 17:22:02 +0530

    new

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Travel</title>
 </head>
 
@@ -16,20 +16,20 @@
             <div class="imt">
                 <img src="images/logo.svg" alt="" width="150px" height="150px">
             </div>
-        
-        <ul>
-            <a href="home.html">Home</a>
-            <a href="about.html">About</a>
-            <a href="contact.html">Contact Us</a>
-        </ul>
 
-        <div class="log flex-all-center">
-            <form action="/final/loginsystem/signup.php" method="post"><button>Register</button></form>
-            <form action="/final/loginsystem/login.php" method="post"><button>Login</button></form>
-            
-        </div>
+            <ul>
+                <a href="home.html">Home</a>
+                <a href="about.html">About</a>
+                <a href="contact.html">Contact Us</a>
+            </ul>
+
+            <div class="log flex-all-center">
+                <form action="/final/loginsystem/signup.php" method="post"><button>Register</button></form>
+                <form action="/final/loginsystem/login.php" method="post"><button>Login</button></form>
+
+            </div>
         </nav>
-        
+
     </header>
 
     <main>
@@ -94,60 +94,91 @@
             </div>
         </div>
 
-        <div class="cade">
-            <div class="cs">
-                <img class="c4" src="images/img2.jpeg" alt="">
-                <div class="c6">
-                    <h1>Family with kids</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.<br>Nihil animi molestiae deserunt adipisci
-                        reiciendis ex unde vel fugiat. </p>
-                    <div class="button">
+
+        <div class="contain">
+            <div class="cd">
+                <div class="cd1">
+                    <img src="images/img2.jpeg" alt="" srcset="">
+                </div>
+                <div class="cd2">
+                    <div class="cd21">
+                        <h1>Family with kids</h1>
+                    </div>
+                    <div class="cd22">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo iusto fuga.</p>
+                    </div>
+                    <div class="cd23">
                         <a href="/final/loginsystem/signup.php">Book Now</a>
                     </div>
                 </div>
             </div>
-            <div class="cs">
-                
-                <div class="c6">
-                    <h1>Family with kids</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.<br>Nihil animi molestiae deserunt
-                        adipisci
-                        reiciendis ex unde vel fugiat. </p>
-                    <div class="button">
+            <div class="cd">
+                <div class="cd2">
+                    <div class="cd21">
+                        <h1>Enjoy a 24-hour beach holiday</h1>
+                    </div>
+                    <div class="cd22">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo iusto fuga.</p>
+                    </div>
+                    <div class="cd23">
                         <a href="/final/loginsystem/signup.php">Book Now</a>
                     </div>
                 </div>
-                <img class="c4" src="images/img3.jpeg" alt="">
+                <div class="cd1">
+                    <img src="images/img3.jpeg" alt="" srcset="">
+                </div>
+
             </div>
-            <div class="cs">
-                <img class="c4" src="images/img4.jpeg" alt="">
-                <div class="c6">
-                    <h1>Family with kids</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.<br>Nihil animi molestiae deserunt
-                        adipisci
-                        reiciendis ex unde vel fugiat. </p>
-                    <div class="button">
+            <div class="cd">
+                <div class="cd1">
+                    <img src="images/img4.jpeg" alt="" srcset="">
+                </div>
+                <div class="cd2">
+                    <div class="cd21">
+                        <h1>Enjoy a 72-hour beach holiday</h1>
+                    </div>
+                    <div class="cd22">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo iusto fuga.</p>
+                    </div>
+                    <div class="cd23">
                         <a href="/final/loginsystem/signup.php">Book Now</a>
                     </div>
+
                 </div>
             </div>
-            <div class="cs">
-                
-                <div class="c6">
-                    <h1>Family with kids</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.<br>Nihil animi molestiae deserunt
-                        adipisci
-                        reiciendis ex unde vel fugiat. </p>
-                    <div class="button">
+            <div class="cd">
+                <div class="cd2">
+                    <div class="cd21">
+                        <h1>Enjoy more than three days of an ideal beach holiday</h1>
+                    </div>
+                    <div class="cd22">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quo iusto fuga.</p>
+                    </div>
+                    <div class="cd23">
                         <a href="/final/loginsystem/signup.php">Book Now</a>
                     </div>
+
                 </div>
-                <img class="c4" src="images/img5.jpeg" alt="">
+                <div class="cd1">
+                    <img src="images/img5.jpeg" alt="" srcset="">
+                </div>
+
             </div>
+        </div>
+
+
+        <div class="container">
+            <h1><b>Our Location</b></h1>
+            <p class="flex-all-center"><iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.805522438118!2d72.99448191421405!3d19.159988354307615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7bf4daf8967d9%3A0xdd90bed2058f7eaa!2sDatta%20Meghe%20College%20Of%20Engineering!5e0!3m2!1sen!2sin!4v1679913241206!5m2!1sen!2sin"
+                    width="1000" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe></p>
+
+        </div>
     </main>
 
     <footer class="flex-all-center">
-        <p>Copyright &copy; MyCart.com </p>
+        <p>Copyright &copy; Batch2.com </p>
     </footer>
 
 </body>
