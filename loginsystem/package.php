ae9451aa9d14630c34dba637549ec770a02ee294
OM1705 <ompol1705@gmail.com>
2023-03-29 20:43:40 +0530

    new3

--- loginsystem/package.php
+++ loginsystem/package.php
@@ -15,7 +15,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/pk.css">
+    <link rel="stylesheet" href="pk.css">
     <title>Package</title> 
 </head>
 
